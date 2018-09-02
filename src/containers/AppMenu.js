41fdb8255708c923572e35aa965432f8a9604446
Brian Harbron <bharbron@grubhub.com>
2018-09-02 16:44:36 -0500

    Different ordering for the AppMenu. Put the logo at the bottom.

--- src/containers/AppMenu.js
+++ src/containers/AppMenu.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import { push } from 'connected-react-router'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom';
 import {
   Divider,
   Header,
   Icon,
   List,
   Menu,
-  Segment
 } from 'semantic-ui-react';
 import routes from '../constants/routes.json'
 
@@ -33,10 +31,6 @@ class AppMenu extends Component {
   render () {
     return (
       <Menu secondary pointing size='large' fixed='left' vertical id='AppMenu'>
-        <Header as='h2' textAlign='center' icon inverted color='grey' className='AppLogo'>
-          <Icon name='cubes' />
-          HexPop!
-        </Header>
         <Menu.Item active={ this.isActive(routes.HEXES) } onClick={() => this.props.changePage(routes.HEXES)}><Icon name='cube' className='left' />Hexes</Menu.Item>
         <Menu.Item active={ this.isActive(routes.TAGS) } onClick={() => this.props.changePage(routes.TAGS)}><Icon name='tags' className='left' />Tags</Menu.Item>
         <Menu.Item active={ this.isActive(routes.TABLES) } onClick={() => this.props.changePage(routes.TABLES)}><Icon name='list' className='left' />Tables</Menu.Item>
@@ -48,6 +42,10 @@ class AppMenu extends Component {
           <List.Item style={{ fontSize: '10pt', color: 'lightgrey' }}><span style={{ fontSize: '12pt', fontFamily: 'courier', color: 'darkgrey' }}>[[CODE:+tag1,-tag2]]</span> Add tag1 to the filter, remove tag2, roll on CODE, insert result</List.Item>
           <List.Item style={{ fontSize: '10pt', color: 'lightgrey' }}><span style={{ fontSize: '12pt', fontFamily: 'courier', color: 'darkgrey' }}>[[CODE:-ALL,+tag1]]</span> Remove all tags from the filter, add tag1, roll on CODE, insert result</List.Item>
         </List>
+        <Header as='h1' textAlign='center' icon inverted color='grey' className='AppLogo'>
+          <Icon name='cubes' />
+          HexPop!
+        </Header>
       </Menu>
     )
   }
