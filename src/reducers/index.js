37aa276bb73f83b78c7bd8baf51b37ba69ac9158
Brian Harbron <bharbron@grubhub.com>
2018-09-04 12:31:20 -0500

    Got deletion workinggit diff Problem was with the reducers. It looks like for deletion, making a copy of the original state then mutating the copy is a bad idea. Reworked all of my reducers into Redux's preferred three-dot spread syntax.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,24 +4,45 @@ import { nextIntegerId } from '../helpers'
 
 function add_hex_detail(state, action) {
   const hex_detail_id = nextIntegerId(state.entry_details.allIds)
-      /* Make copy of the state so we don't mutate existing state. Redux hates that. */
-      const new_state = Object.assign({}, state)
-
-      /* Mutate the new state */
-      new_state.tables.byId["HEX"].global_entry_details.push(hex_detail_id)
-      new_state.entry_details.byId[hex_detail_id] = {id: hex_detail_id, text: action.text}
-      new_state.entry_details.allIds.push(hex_detail_id)
-      return new_state
+  const new_state = {...state,
+    tables: {...state.tables,
+      byId: {...state.tables.byId,
+        "HEX": {...state.tables.byId["HEX"],
+          global_entry_details: [...state.tables.byId["HEX"].global_entry_details,
+            hex_detail_id
+          ]
+        }
+      }
+    },
+    entry_details: {...state.entry_details,
+      byId: {...state.entry_details.byId,
+        [hex_detail_id]: {id: hex_detail_id, text: action.text}
+      },
+      allIds: [...state.entry_details.allIds,
+        hex_detail_id
+      ]
+    }
+  }
+  return new_state
 }
 
 function delete_hex_detail(state, action) {
-  const new_state = Object.assign({}, state)
-
-  let index = new_state.tables.byId["HEX"].global_entry_details.indexOf(action.id);
-  new_state.tables.byId["HEX"].global_entry_details.splice(index, 1)
-  delete new_state.entry_details.byId[action.id]
-  index = new_state.entry_details.allIds.indexOf(action.id)
-  new_state.entry_details.allIds.splice(index, 1)
+  const id = action.id
+  const new_state = {...state,
+    tables: {...state.tables,
+      byId: {...state.tables.byId,
+        "HEX": {...state.tables.byId["HEX"],
+          global_entry_details: state.tables.byId["HEX"].global_entry_details.filter(item => item != id)
+        }
+      }
+    },
+    entry_details: {...state.entry_details,
+      allIds: state.entry_details.allIds.filter(item => item != id),
+      byId: {...state.entry_details.byId,
+        id: undefined
+      }
+    }
+  }
   return new_state
 }
 
