0681d48a910402f1351920900b911ec4b56f50e2
Brian Harbron <bharbron@grubhub.com>
2018-09-30 13:17:33 -0500

    Started implementing reducers for UPDATE_HEX action

--- src/reducers/entrydetailsgroups.js
+++ src/reducers/entrydetailsgroups.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux'
 import {arrayWithPush} from './helpers'
-import {ADD_HEX_DEFINITION, DELETE_HEX_DEFINITION} from '../actions/hexes'
+import {ADD_HEX_DEFINITION, DELETE_HEX_DEFINITION, UPDATE_HEX} from '../actions/hexes'
 import {ADD_TABLE_ENTRY, UPDATE_TABLE_ENTRY} from '../actions/tabledetails'
 
 function byId(state=null, action) {
@@ -9,6 +9,7 @@ function byId(state=null, action) {
   switch (action.type) {
     case ADD_HEX_DEFINITION: return byIdAddHexDefinition(state, action)
     case DELETE_HEX_DEFINITION: return byIdDeleteHexDefinition(state, action)
+    case UPDATE_HEX: return byIdUpdateHex(state, action)
     case ADD_TABLE_ENTRY: return byIdAddTableEntry(state, action)
     case UPDATE_TABLE_ENTRY: return byIdUpdateTableEntry(state, action)
     default: return state
@@ -19,6 +20,7 @@ function allIds(state=null, action) {
   console.log(state)
   console.log(action)
   switch (action.type) {
+    case UPDATE_HEX: return allIdsUpdateHex(state, action)
     case ADD_TABLE_ENTRY: return allIdsAddTableEntry(state, action)
     default: return state
   }
@@ -42,6 +44,56 @@ function byIdDeleteHexDefinition(state, action) {
       entryDetails: [...state['HEX'].entryDetails.filter(item => item !== action.payload.entryDetailId)]
     }
   }
+}
+
+function byIdUpdateHex(state, action) {
+  /*
+  1. Either add, remove, or update depending on difference in entryDetailsGroup between hex and prevHex
+  */
+  const entryDetailsGroup = action.payload.hex.entryDetailsGroup
+  const prevEntryDetailsGroup = action.payload.prevHex.entryDetailsGroup
+  const entryDetails = action.payload.hex.entryDetails
+  const prevEntryDetails = action.payload.prevHex.entryDetails
+  if (prevEntryDetailsGroup === 'HEX' && entryDetailsGroup === 'HEX') {
+    // Still using global hex definition, so nothing to do here
+    return state
+  }
+  if (prevEntryDetailsGroup === 'HEX' && entryDetailsGroup !== 'HEX') {
+    // Was previously using global hex definitions, but now overriding those
+    // Need to add the new entryDetailsGroup and populate it with the entryDetails
+    return {
+      ...state,
+      [entryDetailsGroup]: {
+        id: entryDetailsGroup,
+        entryDetails: [...entryDetails]
+      }
+    }
+  }
+  if (prevEntryDetailsGroup !== 'HEX' && entryDetailsGroup === 'HEX') {
+    // Was previously overriding definitions, but have toggled back to using global hex definitions
+    // Need to delete the previous entryDetailsGroup
+    return {
+      ...state,
+      [prevEntryDetailsGroup]: undefined
+    }
+  }
+  if (prevEntryDetailsGroup !== 'HEX' && entryDetailsGroup !== 'HEX') {
+    // Was previously overriding hex definitions, and still are
+    // In theory, prevEntryDetailsGroup and entryDetailsGroup should be equal
+    // due to the way we manage toggling on the UI side
+    // But to be safe, delete prevEntryDetailsGroup, and add the new entryDetailsGroup and populate it with the entryDetails
+    return {
+      ...state,
+      [prevEntryDetailsGroup]: undefined,
+      [entryDetailsGroup]: {
+        id: entryDetailsGroup,
+        entryDetails: [...entryDetails]
+      }
+    }
+  }
+  // If for some reason we didn't catch something earlier
+  console.log("This shouldn't have happened. Reduction of UPDATE_HEX for entrydetailsgroups wasn't caught.")
+  return state
 }
 
 function byIdAddTableEntry(state, action) {
@@ -85,6 +137,11 @@ function byIdUpdateTableEntry(state, action) {
     }
   }
   return newState
+}
+
+function allIdsUpdateHex(state, action) {
+  //TODO TODO
+  return state
 }
 
 function allIdsAddTableEntry(state, action) {
